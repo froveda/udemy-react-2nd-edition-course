34b88073a7a216fe260cdde563d4dc47984d80bf
Fabricio roveda <froveda@gmail.com>
2020-04-13 22:38:05 -0300

    Finished Section 9: React-Router

---
+++ portfolio-site/src/components/ContactPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ContactPage = (props) => (
+  <div>
+    <h1>Contact Me</h1>
+    <p>You can reach me at test@gmail.com</p>
+  </div>
+);
+export default ContactPage;
