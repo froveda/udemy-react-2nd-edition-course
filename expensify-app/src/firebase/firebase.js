6de39f803d18d2b1d4c530b5d80364e9035c85bd
Fabricio roveda <froveda@gmail.com>
2020-04-28 14:37:18 -0300

    Finished Section 14: Firebase 101

--- expensify-app/src/firebase/firebase.js
+++ expensify-app/src/firebase/firebase.js
@@ -12,3 +12,95 @@ const config = {
 };
 
 firebase.initializeApp(config);
+
+const database = firebase.database();
+
+// database.ref('expenses').on('value', (snapshot) => {
+//   const expenses = [];
+
+//   snapshot.forEach(function (childSnapshot) {
+//     expenses.push({
+//       key: childSnapshot.key,
+//       ...childSnapshot.val()
+//     });
+//   });
+
+//   console.log(expenses);
+// });
+
+// child_removed
+database.ref('expenses').on('child_removed', (snapshot) => {
+  console.log(snapshot.key, snapshot.val());
+});
+
+// child_changed
+database.ref('expenses').on('child_changed', (snapshot) => {
+  console.log('Child changed', snapshot.key, snapshot.val());
+});
+
+// child_added
+database.ref('expenses').on('child_added', (snapshot) => {
+  console.log('Child added', snapshot.key, snapshot.val());
+});
+
+// database.ref('expenses').push({
+//   description: 'Gum',
+//   note: '',
+//   amount: 195,
+//   createdAt: 0
+// });
+
+// database.ref('expenses').push({
+//   description: 'Rent',
+//   note: '',
+//   amount: 109500,
+//   createdAt: 4000
+// });
+
+// database.ref('expenses').push({
+//   id: '3',
+//   description: 'Credit Card',
+//   note: '',
+//   amount: 4500,
+//   createdAt: -4000
+// });
+
+// database.ref().set({
+//   name: 'Fabricio Roveda',
+//   age: 39,
+//   isSingle: false,
+//   location: {
+//     city: 'CABA',
+//     country: 'Argentina'
+//   },
+//   job: {
+//     title: 'Sofware Developer',
+//     company: 'Ipsy'
+//   }
+// }).then(() => {
+//   console.log('Data is saved');
+// }).catch((e) => {
+//   console.log('This failed', e);
+// });
+
+// database.ref().on('value', (snapshot) => {
+//   const val = snapshot.val();
+//   const name = val.name;
+//   const jobTitle = val.job.title;
+//   const jobCompany = val.job.company;
+//   console.log(`${name} is a ${jobTitle} at ${jobCompany}`);
+// });
+
+// database.ref().update({
+//   'job/title': 'Software Engineer'
+// });
+
+// database.ref('isSingle').remove()
+//   .then(function () {
+//     console.log("Remove succeeded.")
+//   })
+//   .catch(function (error) {
+//     console.log("Remove failed: " + error.message)
+//   });
+
+// console.log('I made a request to change the data');
