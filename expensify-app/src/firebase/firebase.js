89f818c7388efdbea9ff2af618a143c555a32124
Fabricio roveda <froveda@gmail.com>
2020-04-28 11:32:09 -0300

    Finished 142. Getting Firebase

---
+++ expensify-app/src/firebase/firebase.js
@@ -0,0 +1,14 @@
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: "AIzaSyDoYHgna1IVHqexrujelyRWzlPpUwUispM",
+  authDomain: "react2courseexpensify.firebaseapp.com",
+  databaseURL: "https://react2courseexpensify.firebaseio.com",
+  projectId: "react2courseexpensify",
+  storageBucket: "react2courseexpensify.appspot.com",
+  messagingSenderId: "401267790335",
+  appId: "1:401267790335:web:9fd46485efcb5cacf817ca",
+  measurementId: "G-ESDLLC7ZD4"
+};
+
+firebase.initializeApp(config);
