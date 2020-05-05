ae8225efa6ef37681566bbcf10cac2f0d2b0707a
Fabricio roveda <froveda@gmail.com>
2020-05-05 13:47:54 -0300

    Finish 199. Fragments

--- react-new-features/src/components/Note.js
+++ react-new-features/src/components/Note.js
@@ -9,11 +9,11 @@ const Note = ({ note }) => {
   }
 
   return (
-    <div>
+    <>
       <h3>{note.title}</h3>
       <p>{note.body}</p>
       <button onClick={() => removeNote(note.title)}>x</button>
-    </div>
+    </>
   );
 }
 
