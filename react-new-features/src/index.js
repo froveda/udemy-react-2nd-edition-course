3a3da666abd835506e7bf9e7a963c54dcf88184b
Fabricio roveda <froveda@gmail.com>
2020-05-05 11:02:37 -0300

    Finish 191. useState vs. setState

--- react-new-features/src/index.js
+++ react-new-features/src/index.js
@@ -2,32 +2,75 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
+// We can use this approach, but is better to use useState multiple times
 const App = (props) => {
-  const [count, setCount] = useState(props.count);
+  const [state, setState] = useState({
+    count: props.count,
+    text: ''
+  })
 
   const increment = () => {
-    setCount(count + 1);
+    setState({ ...state, count: state.count + 1 });
   }
 
   const decrement = () => {
-    setCount(count - 1);
+    setState({ ...state, count: state.count - 1 });
   }
 
   const reset = () => {
-    setCount(props.count);
+    setState({ ...state, count: props.count });
+  }
+
+  const setTextFromInput = (e) => {
+    setState({ ...state, text: e.target.value });
   }
 
   return (
     <React.StrictMode>
       <div>
-        <p>The current count is {count}</p>
+        <p>The current {state.text || 'count'} is {state.count}</p>
         <button onClick={increment}>+1</button>
         <button onClick={decrement}>-1</button>
         <button onClick={reset}>reset</button>
+        <input value={state.text} onChange={setTextFromInput} />
       </div>
     </React.StrictMode>
   )
 }
+
+
+// const App = (props) => {
+//   const [count, setCount] = useState(props.count);
+//   const [text, setText] = useState('');
+
+//   const increment = () => {
+//     setCount(count + 1);
+//   }
+
+//   const decrement = () => {
+//     setCount(count - 1);
+//   }
+
+//   const reset = () => {
+//     setCount(props.count);
+//   }
+
+//   const setTextFromInput = (e) => {
+//     setText(e.target.value);
+//   }
+
+//   return (
+//     <React.StrictMode>
+//       <div>
+//         <p>The current {text || 'count'} is {count}</p>
+//         <button onClick={increment}>+1</button>
+//         <button onClick={decrement}>-1</button>
+//         <button onClick={reset}>reset</button>
+//         <input value={text} onChange={setTextFromInput} />
+//       </div>
+//     </React.StrictMode>
+//   )
+// }
 
 App.defaultProps = {
   count: 0
