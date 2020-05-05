e5c0cc8d857fa72b97ca7833843ae1c1a5fa5f02
Fabricio roveda <froveda@gmail.com>
2020-05-05 11:45:15 -0300

    Finish 193. The useEffect Hook

--- react-new-features/src/index.js
+++ react-new-features/src/index.js
@@ -1,11 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
 const NoteApp = () => {
-  const [notes, setNotes] = useState([]);
+  const notesData = JSON.parse(localStorage.getItem('notes'));
+  const [notes, setNotes] = useState(notesData || []);
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
+
+  useEffect(() => {
+    const parsedNotes = JSON.stringify(notes);
+    localStorage.setItem('notes', parsedNotes);
+  });
 
   const addNote = (e) => {
     e.preventDefault();
